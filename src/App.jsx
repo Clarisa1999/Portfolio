067adcaf7e91994c3015f8039b82bd9f641d7bc4
Saikou Ceesay <ceesaysaikou72@gmail.com>
2022-09-01 19:35:58 -0400

    app theme implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import LandingPage from "./components/landing/LandingPage";
 import About from "./components/about/About";
 import WorkExperience from "./components/experience/WorkExperience";
 import Navbar from "./components/navbar/Navbar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+/*Common Theme configuration for both dark and light mode */
+const commonTheme = {
+  typography: {
+    fontSize: 12,
+  },
+};
+
+/*Light Theme configuration */
+const lightTheme = createTheme({
+  ...commonTheme,
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#62eb31",
+    },
+    secondary: {
+      main: "#e4f2f4",
+    },
+    background: {
+      paper: "white",
+      default: "#EEEEEE",
+    },
+    text: {
+      primary: "#000",
+    },
+  },
+});
+
+/*Dark Theme configuration */
+const darkTheme = createTheme({
+  ...commonTheme,
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#0B1A3F",
+    },
+    secondary: {
+      main: "#fff",
+    },
+    background: {
+      paper: "#001E3C",
+      default: "#0A1929",
+    },
+    text: {
+      primary: "#fff",
+    },
+  },
+});
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+  const globalTheme = theme === "light" ? lightTheme : darkTheme;
   return (
-    <>
-      <Navbar />
+    <ThemeProvider theme={globalTheme}>
+      <Navbar setThme={setTheme} theme={theme} />
       <LandingPage />
       <About />
       <WorkExperience />
-    </>
+    </ThemeProvider>
   );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { menuItems } from "./menuItems";
 import "./navbar.css";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="NavbarItems">
-        <div id="logo">Rue</div>
-        <div id="items">
-          {menuItems.map((item, index) => {
-            return (
-              <a key={index} className={item.cName} href={item.url}>
-                {item.title}
-              </a>
-            );
-          })}
-        </div>
+const Navbar = (props) => {
+  // TODO: implement switch between light and dark mode
+  const { theme, setTheme } = props;
+  return (
+    <div className="NavbarItems">
+      <div id="logo">Rue</div>
+      <div id="items">
+        {menuItems.map((item, index) => {
+          return (
+            <a key={index} className={item.cName} href={item.url}>
+              {item.title}
+            </a>
+          );
+        })}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Navbar;
