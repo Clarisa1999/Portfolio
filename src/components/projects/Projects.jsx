a125a2dcd9648754a7ce99c64328e332232ddef4
Rufaro Clarisa Gapare <rufaroclarisagapare@Rufaros-MacBook-Air.local>
2022-09-05 16:27:09 -0400

    finished contact page design and dark modes

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -25,22 +25,16 @@ export default function Projects() {
         Projects I have done...
       </Typography>
       <Eachpro
-        heading={"McGill Space"}
-        body={
-          "Yourself could be a challenging task, but when it comes to an assignment, you can’t skip it. So, are currently dealing with an essay that requires you to describe yourself? Are you finding it challenging to do so? If so, we are here to help. We understand how normal it is to go blank when asked about describing oneself. And, if you’re a student, the pressure to do so could be immense. It is because you know the reader would be marking your write-up rather than reading it for fun. The pressure to write better and follow the right structure can be overwhelming. It can end up making you freeze or feel you’re not good enough. However, the reality is otherwise. Here are a few things you can do to avoid being in that situation:"
-        }
+        heading="McGill Space"
+        body="Yourself could be a challenging task, but when it comes to an assignment, you can’t skip it. So, are currently dealing with an essay that requires you to describe yourself? Are you finding it challenging to do so? If so, we are here to help. We understand how normal it is to go blank when asked about describing oneself. And, if you’re a student, the pressure to do so could be immense. It is because you know the reader would be marking your write-up rather than reading it for fun. The pressure to write better and follow the right structure can be overwhelming. It can end up making you freeze or feel you’re not good enough. However, the reality is otherwise. Here are a few things you can do to avoid being in that situation:"
       />
       <Eachpro
-        heading={"Image"}
-        body={
-          "Yourself could be a challenging task, but when it comes to an assignment, you can’t skip it. So, are currently dealing with an essay that requires you to describe yourself? Are you finding it challenging to do so? If so, we are here to help. We understand how normal it is to go blank when asked about describing oneself. And, if you’re a student, the pressure to do so could be immense. It is because you know the reader would be marking your write-up rather than reading it for fun. The pressure to write better and follow the right structure can be overwhelming. It can end up making you freeze or feel you’re not good enough. However, the reality is otherwise. Here are a few things you can do to avoid being in that situation:"
-        }
+        heading="Image"
+        body="Yourself could be a challenging task, but when it comes to an assignment, you can’t skip it. So, are currently dealing with an essay that requires you to describe yourself? Are you finding it challenging to do so? If so, we are here to help. We understand how normal it is to go blank when asked about describing oneself. And, if you’re a student, the pressure to do so could be immense. It is because you know the reader would be marking your write-up rather than reading it for fun. The pressure to write better and follow the right structure can be overwhelming. It can end up making you freeze or feel you’re not good enough. However, the reality is otherwise. Here are a few things you can do to avoid being in that situation:"
       />
       <Eachpro
-        heading={"G-City"}
-        body={
-          "Yourself could be a challenging task, but when it comes to an assignment, you can’t skip it. So, are currently dealing with an essay that requires you to describe yourself? Are you finding it challenging to do so? If so, we are here to help. We understand how normal it is to go blank when asked about describing oneself. And, if you’re a student, the pressure to do so could be immense. It is because you know the reader would be marking your write-up rather than reading it for fun. The pressure to write better and follow the right structure can be overwhelming. It can end up making you freeze or feel you’re not good enough. However, the reality is otherwise. Here are a few things you can do to avoid being in that situation:"
-        }
+        heading="G-City"
+        body="Yourself could be a challenging task, but when it comes to an assignment, you can’t skip it. So, are currently dealing with an essay that requires you to describe yourself? Are you finding it challenging to do so? If so, we are here to help. We understand how normal it is to go blank when asked about describing oneself. And, if you’re a student, the pressure to do so could be immense. It is because you know the reader would be marking your write-up rather than reading it for fun. The pressure to write better and follow the right structure can be overwhelming. It can end up making you freeze or feel you’re not good enough. However, the reality is otherwise. Here are a few things you can do to avoid being in that situation:"
       />
     </Box>
   );
